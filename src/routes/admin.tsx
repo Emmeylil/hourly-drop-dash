fede254c41afc78d597792d5094bfd38c2c1cf76
emmeylil <emmanuel.salako@jumia.com>
2026-05-29 10:39:30 +0100

    Fix syntax error in admin.tsx

--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -194,6 +194,7 @@ function Admin() {
               <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M9 5l7 7-7 7" /></svg>
             </button>
           </div>
+          </div>
         </header>
 
         {error && (
